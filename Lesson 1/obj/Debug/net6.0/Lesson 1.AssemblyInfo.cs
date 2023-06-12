098a03cb6fa707295158469bd585538f7f605a76
Cirill <c.cuzi@gmail.ru>
2023-06-12 18:01:45 +0300

    hello

--- Lesson 1/obj/Debug/net6.0/Lesson 1.AssemblyInfo.cs
+++ Lesson 1/obj/Debug/net6.0/Lesson 1.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Hello world")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Lesson 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Hello world")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Hello world")]
+[assembly: System.Reflection.AssemblyProductAttribute("Lesson 1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Lesson 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
